271ad6c6c28c1983bede7eb57f584af364eb804d
adam burdett <adamburdett@tedtested.local>
2020-07-25 16:47:04 -0600

    first/second row generating working slowly

--- src/pairs.jl
+++ src/pairs.jl
@@ -1,6 +1,6 @@
 using Distributed
 using Combinatorics
-using Redis
+#using Redis
 using ProgressMeter
 using JLD
 
@@ -22,8 +22,7 @@ end
 
 function combos(first_rows::Array{Array{Int64,1},1},
                 first_rows_size::Int64, 
-                processed::Int64, 
-                redisClient::Any )
+                processed::Int64)
     #pairs = []
     @showprogress for i = 1 : first_rows_size #every possible first row
         i < processed && continue #processed row
@@ -36,8 +35,8 @@ function combos(first_rows::Array{Array{Int64,1},1},
         #    is_duko(first_rows[i], first_rows[j])
         #end
         #println(length(pairs))
-        #key = join(string.(first_rows[i]),',')
-        #save(joinpath(@__DIR__,"",key,".jld"), key, pairs)
+        key = join(string.(first_rows[i]))
+        save(joinpath(@__DIR__,"sets/",join([key,".jld"])), key, pairs)
     end
 end
 
@@ -45,7 +44,7 @@ function main()
     n = 960
     script_dir = @__DIR__
 
-    redisClient = RedisConnection() # host=127.0.0.1, port=6379, db=0, no password
+    #redisClient = RedisConnection() # host=127.0.0.1, port=6379, db=0, no password
     #pipe = open_pipeline(redisClient)
 
     first_rows = Combinatorics.permutations([1,2,3,4,5,6,7,8,9]) |> collect 
@@ -55,7 +54,7 @@ function main()
     processed = 0# if processed is None ? 0 : parse(Int8,processed)
     #println(processed)
     #print(comb(362880,2))
-    combos(first_rows,first_rows_size, processed, redisClient )
+    combos(first_rows,first_rows_size, processed)
     println("------------ completed ------------")
 end
 
